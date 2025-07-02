27adaead2b1953ddfb59d32fa97dbccf33250d9c
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2025-07-01 21:08:33 -0300

    try connect to render v1.0

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,10 +3,12 @@ import { CorsOptions } from "cors";
 export const corsConfing: CorsOptions = {
   origin: function (origin, callback) {
     const whiteList = [process.env.FRONTEND_URL];
-    console.log(origin);
+
     if (process.argv[2] === "--api") {
       whiteList.push(undefined);
     }
+    console.log("Origen:", origin);
+
     if (whiteList.includes(origin)) {
       callback(null, true);
     } else {
